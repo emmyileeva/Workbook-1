ac9543e1376ce970cbf414acd94b404eeb3543e1
Emiliya Ileeva <emilyileeva@gmail.com>
2025-04-08 14:17:33 -0700

    completed ShoppingList app

---
+++ ShoppingList/src/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My Shopping List:");
+        System.out.println("1. Apples");
+        System.out.println("2. Bread");
+        System.out.println("3. Milk");
+        System.out.println("4. Eggs");
+        System.out.println("5. Cheese");
+        System.out.println("6. Chicken");
+        System.out.println("7. Rice");
+        System.out.println("8. Tomatoes");
+        System.out.println("9. Coffee");
+        System.out.println("10. Chocolate");
+    }
+}
