8d2dda099fbc37774fc6641135cd2cef239ae598
Emiliya Ileeva <emilyileeva@gmail.com>
2025-04-14 11:30:22 -0700

    Completed Mod3 - Exercise 3 - Basic Calculator - Refactor code

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -5,31 +5,53 @@ public class PayrollCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Get user's name
-        System.out.println("Enter your name: ");
-        String name = input.nextLine();
+        // Get user's information using helper methods
+        String name = getName(input);
+        float hoursWorked = getHoursWorked(input);
+        float payRate = getPayRate(input);
 
-        // Get hours worked for user
-        System.out.println("Enter hours worked: ");
-        float hoursWorked = input.nextFloat();
+        // Calculate gross pay based on horus and pay rate
+        float grossPay = calculateGrossPay(hoursWorked, payRate);
 
-        // Get user's pay rate
-        System.out.println("Enter pay rate: ");
-        float payRate = input.nextFloat();
-
-        // Calculate user's pay
-        float grossPay;
-        if (hoursWorked > 40) {
-            float overtimeHours = hoursWorked - 40;
-            grossPay = (40 * payRate) + (overtimeHours * payRate * 1.5f);
-        } else {
-            grossPay = hoursWorked * payRate;
-        }
-
-        // Output the result
-        System.out.println("\nEmployee Name: " + name);
-        System.out.println("Gross Pay: " + grossPay);
+        // Display the final result
+        displayResults(name, grossPay);
 
         input.close();
     }
+
+    // Method to get user's name
+    public static String getName(Scanner input) {
+        System.out.println("Enter your name: ");
+        return input.nextLine();
+    }
+
+    // Method to get hours worked from the user
+    public static float getHoursWorked(Scanner input) {
+        System.out.println("Enter hours worked: ");
+        return input.nextFloat();
+    }
+
+    // Method to get pay rate from user
+    public static float getPayRate(Scanner input) {
+        System.out.println(" Enter your pay rate: ");
+        return input.nextFloat();
+    }
+
+    // Method to calculate gross pay with overtime logic
+    public static float calculateGrossPay(float hours, float rate) {
+        if (hours > 40) {
+            float overtimeHours = hours - 40;
+            return (40 * rate) + (overtimeHours * rate * 1.5f); // Overtime paid at 1.5x
+        } else {
+            return hours * rate; // Regular pay
+        }
+    }
+
+    // Method to print the final payroll summary
+    public static void displayResults(String name, float grossPay) {
+        System.out.println("\n---- Payroll Summary ----");
+        System.out.println("Employee Name: " + name);
+        System.out.printf("Gross Pay: $%.2f\n", grossPay);
+    }
+
 }
